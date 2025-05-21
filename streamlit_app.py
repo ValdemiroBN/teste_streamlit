b1db2000b5241e154c1302d6ca13d0bd9158f4df
Miro <89658246+ValdemiroBN@users.noreply.github.com>
2025-05-21 15:52:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from PIL import Image
 
+st.title("Upload e visualização de imagem")
+
+uploaded_file = st.file_uploader("Escolha uma imagem", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Imagem carregada", use_column_width=True)
+else:
+    st.write("Aguardando upload...")
